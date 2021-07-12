a02f12877e4e1a5608d9e057aa811f97c7167053
ElianoPereira <ejeulin@outlook.fr>
2021-07-12 18:54:48 +0200

    Initial commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    public float speed;
+
+    void Start()
+    {
+        transform.position = new Vector3(RandomInt()/100, RandomInt()/100, RandomInt()/100);
+        transform.localScale = Vector3.one * RandomInt()/RandomInt();
+        
+        Material material = Renderer.material;
+
+        speed = RandomInt();
+
+        material.color = new Color(RandomInt()/255, RandomInt()/255, RandomInt()/255, RandomInt()/255);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime, speed * Time.deltaTime, 0.0f);
+    }
+
+    float RandomInt()
+    {
+        return Random.Range(0, 255);
+    }
+}
---
+++ Assets/ModTheCube/cameraMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMvt = Input.GetAxis("Horizontal");
+        float verticalMvt = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * horizontalMvt);
+        transform.Translate(Vector3.up * speed * Time.deltaTime * verticalMvt);
+        transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+}
